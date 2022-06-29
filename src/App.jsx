53cb134e90803a2215a3b59ca9f499d44c13ca0d
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-06-29 02:46:55 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+import Spline from '@splinetool/react-spline';
+
+function App() {
+  return (
+   <main> 
+     <div class="d">
+        <Spline scene="https://prod.spline.design/NQQ8fiPn-eY0cLsO/scene.splinecode" /></div>
+  <button class ="work_button">my work</button>
+   </main>
+    
+  );
+}
+
+export default App;
+
+
+
