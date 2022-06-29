127bd21b6679249822bfde7419ae64025ca19781
Tanjila Kabir <70307218+tanjilak@users.noreply.github.com>
2022-06-29 20:04:31 +0000

    added nav bar and updated mobile layout

--- script.js
+++
@@ -1,8 +0,0 @@
-function detectDevice({
-  let type = new MobileDetect(window.navigator.userAgent)
-
-  let spline = document.getElementByClassName("section1");
-  if (type.os()==="IOS"){
-    section1.innerHTML = <Spline scene="https://prod.spline.design/HXych7uKwfhCDwfA/scene.splinecode" />;
-  }
-})
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,59 @@
 import React from 'react';
 import './App.css';
 
+
 import Spline from '@splinetool/react-spline';
 
 function App() {
   return (
+    
+
+    
    <main> 
-     <div className="section1">
-        <Spline scene="https://prod.spline.design/NQQ8fiPn-eY0cLsO/scene.splinecode" />
-      
-     </div>
- <a href="#section2"><button className ="work_button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M4 9L12 17L20 9" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+
+    <nav className="menu-container">
+
+  
+  <div className="menu">
+    <ul>
+      <li>
+        <a href="https://github.com/tanjilak" target="_blank" rel="noopener noreferrer">
+          github
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/tanjila-kabir/" target="_blank" rel="noopener noreferrer">
+          linkedin
+        </a>
+      </li>
+      <li>
+        <a href="https://dribbble.com/tanjiva" target="_blank" rel="noopener noreferrer">
+          dribble
+        </a>
+      </li>
+    </ul>
+    <ul>
+    </ul>
+  </div>
+</nav>
+    
+     <a href="#section2"><button className ="work_button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4 9L12 17L20 9" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 
 
 </button></a>
+     
+     <div className="section1">
+        <Spline id="first" scene="https://draft.spline.design/tbi07Y8kTTx1e1nq/scene.splinecode" />
+        <Spline id ="second" scene="https://prod.spline.design/BSlJaeqX6iyvWq1T/scene.splinecode" />
+
+            
+ 
+
+     </div>
+
+     
      <div id="section2">
   
      <p className="links">designs</p>
